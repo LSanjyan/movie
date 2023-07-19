a9b0965d3b4c633a099817595aaf113d445adc6a
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2023-07-19 16:24:00 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import MovieDisplay from './components/MovieDisplay';
+import Form from './components/Form';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const movieList = [
+    "Donnie Darko",
+    "Home Alone",
+    "Black Panther",
+    "The Dark Knight",
+    "Spider-Man"
+  ]
+
+  const apiKey = "40dc040"
+
+  const [movie, setMovie] = useState(null)
+
+  const getMovie = async (searchTerm) => {
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      )
+
+      const data = await response.json()
+
+      setMovie(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getMovie(movieList[Math.floor(Math.random() * movieList.length)]);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
